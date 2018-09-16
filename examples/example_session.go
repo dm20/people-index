717b44d021a438e1a64b48d98a9e48ebb2c7b791
Daniel <demcgrath2@gmail.com>
2018-09-16 00:22:35 -0700

    Update example_session.go

--- examples/example_session.go
+++ examples/example_session.go
@@ -2,6 +2,12 @@ package main
 
 import "github.com/dm20/people_index"
 
+func main() {
+  people_index.Initialize()   // open 'people.json' and get existing file data, or create it if needed
+  people_index.RunTests()     // see RunTests() in people_index.go
+  people_index.SaveAndExit()  // replace old json data with updated json data in 'people.json'
+}
+
 // 1) make sure to put the interface in $GOPATH : 
 //       go get github.com/dm20/people_index
 //
@@ -10,9 +16,3 @@ import "github.com/dm20/people_index"
 //
 // 2) go build example_session.go
 // 3) go run example_session.go
-
-func main() {
-  people_index.Initialize()   // open 'people.json' and get existing file data, or create it if needed
-  people_index.RunTests()     // see RunTests() in people_index.go
-  people_index.SaveAndExit()  // replace old json data with updated json data in 'people.json'
-}
