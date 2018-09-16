c31dd9aa3cbc445472f660698b0932b180a1e55b
Daniel <demcgrath2@gmail.com>
2018-09-15 18:24:29 -0700

    Update example_session.go

--- examples/example_session.go
+++ examples/example_session.go
@@ -3,7 +3,7 @@ package main
 import "github.com/dm20/people_index"
 
 // 1) make sure to put the interface in $GOPATH : 
-//        go get github.com/dm20/people_index
+//       go get github.com/dm20/people_index
 //
 //    and to get the dependencies: 
 //        go get github.com/Jeffail/gabs
